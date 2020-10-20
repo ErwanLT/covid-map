8d014264faa191cca39726cd817caea236988c00
Erwan <erwanletutour.elt@gmail.com>
2020-10-20 10:12:36 +0200

    entichissement pop up + data qualitie

--- src/app/services/pop-up.service.ts
+++ src/app/services/pop-up.service.ts
@@ -11,8 +11,9 @@ export class PopUpService {
     return `` +
       `<div><span style="font-weight: bold;">Pays: </span>${ data.properties.country }</div>` +
       `<div><span style="font-weight: bold;">Capitale: </span>${ data.properties.city }</div>` +
-      `<div><span style="font-weight: bold;">Cas: </span>${ data.activeCase }</div>` +
-      `<div><span style="font-weight: bold;">Mort: </span>${ data.deaths }</div>` +
-      `<div><span style="font-weight: bold;">Guéris: </span>${ data.recovered }</div>`;
+      `<div><span style="font-weight: bold;">Nombre total de cas: </span>${ data.totalCase }</div>` +
+      `<div><span style="font-weight: bold;">Cas actif: </span>${ data.activeCase }</div>` +
+      `<div><span style="font-weight: bold;">Décès: </span>${ data.deaths }</div>` +
+      `<div><span style="font-weight: bold;">Guérison: </span>${ data.recovered }</div>`;
   }
 }
