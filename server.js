6d718a57b8b5d8dadb806a429cab6157ed9baae2
Erwan <erwanletutour.elt@gmail.com>
2020-10-20 09:15:57 +0200

    change to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/covid-map'));
 
-app.get('/*', function(req,res) {
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/covid-map/'}),
+);
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
